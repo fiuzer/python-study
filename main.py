43f20a80822c84eff6681c823ee9e683f48109d4
Guilherme Fiuza <guigofiuza@gmail.com>
2024-10-12 01:47:43 -0300

    Atualização do relógio

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from time import sleep
 from datetime import datetime
+
 def relogio():
-  while True:
-    print("Hora:", end=" ")
-    current_time = datetime.now().strftime("%H:%M:%S")
-    print(current_time)
-    sleep(1)
-relogio()
+    """
+    Exibe a hora atual a cada segundo até que o usuário interrompa o programa.
+    """
+    try:
+        while True:
+            current_time = datetime.now().strftime("%H:%M:%S")
+            print(f"\rHora: {current_time}", end="", flush=True)
+            sleep(1)
+    except KeyboardInterrupt:
+        print("\nPrograma encerrado pelo usuário.")
+
+if __name__ == "__main__":
+    relogio()
